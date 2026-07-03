68f5b1926ee52a0a78de6a9916edd7e20eef2380
DevWalrus <70732411+DevWalrus@users.noreply.github.com>
2026-07-03 16:05:16 -0400

    Add stage to infra and ymls

--- infra/static_web_app.tf
+++ infra/static_web_app.tf
@@ -1,3 +1,16 @@
+resource "azurerm_static_web_app" "creator_ui_stage" {
+  name                = "${var.project}-creator-ui-stage"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = "eastus2"
+  sku_tier            = "Free"
+  sku_size            = "Free"
+
+  app_settings = {
+    CREATOR_API_URL = "https://${azurerm_container_app.creator_api_stage.ingress[0].fqdn}"
+    MONGODB_URI     = azurerm_cosmosdb_account.main.primary_mongodb_connection_string
+  }
+}
+
 resource "azurerm_static_web_app" "creator_ui" {
   name                = "${var.project}-creator-ui"
   resource_group_name = azurerm_resource_group.main.name
